24abeebc987bfc1218710ae83d4e82513f2fd831
aquamary <m.vlasova@astrovolga.ru>
2022-11-20 15:33:33 +0400

    module7-task1 добавит поддержку токенов

--- project/src/services/api.ts
+++ project/src/services/api.ts
@@ -1,4 +1,5 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
+import { getToken } from './token';
 
 const BACKEND_URL = 'https://11.react.pages.academy/six-cities-simple';
 const REQUEST_TIMEOUT = 5000;
@@ -9,5 +10,17 @@ export const createAPI = (): AxiosInstance => {
     timeout: REQUEST_TIMEOUT,
   });
 
+  api.interceptors.request.use(
+    (config: AxiosRequestConfig) => {
+      const token = getToken();
+
+      if (token && config.headers) {
+        config.headers['x-token'] = token;
+      }
+
+      return config;
+    },
+  );
+
   return api;
-}
+};

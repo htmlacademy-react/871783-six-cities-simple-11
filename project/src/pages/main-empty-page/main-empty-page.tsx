c162a9b07c951f429173f474535884584125eca1
aquamary <m.vlasova@astrovolga.ru>
2022-11-26 22:17:10 +0400

    module7-task1 подключает CityList к MainEmptyPage

--- project/src/pages/main-empty-page/main-empty-page.tsx
+++ project/src/pages/main-empty-page/main-empty-page.tsx
@@ -1,7 +1,13 @@
-import { Header } from '../../components';
+import {CityList, Header} from '../../components';
 import { Helmet } from 'react-helmet-async';
+import { changeCityAction } from '../../store/action';
+import { useAppDispatch, useAppSelector } from '../../hooks';
 
 function MainEmptyPage(): JSX.Element {
+  const dispatch = useAppDispatch();
+  const selectedCity = useAppSelector((state) => state.city);
+  const handleCitySelect = (city: string) => dispatch(changeCityAction(city));
+
   return (
     <div className="page page--gray page--main">
       <Helmet>
@@ -13,38 +19,10 @@ function MainEmptyPage(): JSX.Element {
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
           <section className="locations container">
-            <ul className="locations__list tabs__list">
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Paris</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Cologne</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Brussels</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item">
-                  <span>Amsterdam</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Hamburg</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item tabs__item--active" href="#">
-                  <span>Dusseldorf</span>
-                </a>
-              </li>
-            </ul>
+            <CityList
+              selectedCity={ selectedCity }
+              onCityClick={ handleCitySelect }
+            />
           </section>
         </div>
         <div className="cities">

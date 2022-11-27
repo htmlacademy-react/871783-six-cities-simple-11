fcb8a0137186610d87ad64742ba0c377da1ef89f
aquamary <m.vlasova@astrovolga.ru>
2022-11-27 14:37:11 +0400

    module7-task1 добавляет сортировку отззывов по дате

--- project/src/components/review-list/review-list.tsx
+++ project/src/components/review-list/review-list.tsx
@@ -6,9 +6,19 @@ type ReviewListProps = {
 }
 
 function ReviewList({reviews}: ReviewListProps): JSX.Element {
+  let sortedReviews: Reviews = [];
+
+  if (reviews.length) {
+    sortedReviews = [...reviews]
+      .sort((a, b) => new Date(b.date).valueOf() - new Date(a.date).valueOf())
+      .slice(0, 10);
+  } else {
+    sortedReviews = reviews;
+  }
+
   return (
     <ul className="reviews__list">
-      { reviews.map((review) => (
+      { sortedReviews.map((review) => (
         <UserReview
           review={ review }
           key={ review.id }

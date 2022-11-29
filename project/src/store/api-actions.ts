b50ac7f3cbe841d351f5c87fdf26e3671260c3c5
aquamary <m.vlasova@astrovolga.ru>
2022-11-29 08:47:56 +0400

    module7-task1 фиксит вывод данных user

--- project/src/store/api-actions.ts
+++ project/src/store/api-actions.ts
@@ -9,13 +9,14 @@ import {
   requireAuthorization,
   setCommentsAction,
   setLoadingStatus,
+  loadCurrentOfferAction, getUserData,
 } from './action';
 import { saveToken, dropToken } from '../services/token';
 import { APIRoute, AuthorizationStatus } from '../const';
 import { AuthData } from '../types/auth-data';
 import { UserData } from '../types/user-data';
 import { AppRoute } from '../router';
-import { Reviews } from '../types/review';
+import {ReviewData, Reviews} from '../types/review';
 
 export const fetchOffersAction = createAsyncThunk<void, undefined, {
   dispatch: AppDispatch;
@@ -24,10 +25,28 @@ export const fetchOffersAction = createAsyncThunk<void, undefined, {
 }>(
   'data/fetchOffers',
   async (_arg, {dispatch, extra: api}) => {
-    dispatch(setLoadingStatus(true));
+    // dispatch(setLoadingStatus(true));
     const {data} = await api.get<Offer[]>(APIRoute.Offers);
-    dispatch(setLoadingStatus(false));
+
     dispatch(loadOffersAction(data));
+    // dispatch(setLoadingStatus(false));
+  },
+);
+
+export const fetchOfferAction = createAsyncThunk<void, number, {
+  dispatch: AppDispatch;
+  state: State;
+  extra: AxiosInstance;
+}>(
+  'data/fetchCurrentOffer',
+  async (id, {dispatch, extra: api}) => {
+    try {
+      const {data} = await api.get<Offer>(`${APIRoute.Offers}/${id}`);
+      dispatch(loadCurrentOfferAction(data));
+    }
+    catch {
+      dispatch(redirectToRoute(AppRoute.NotFound));
+    }
   },
 );
 
@@ -38,9 +57,7 @@ export const fetchOffersNearbyAction = createAsyncThunk<void, number, {
 }>(
   'data/fetchNearbyOffers',
   async (id, {dispatch, extra: api}) => {
-    dispatch(setLoadingStatus(true));
     const {data} = await api.get<Offer[]>(`${APIRoute.Offers}/${id}/nearby`);
-    dispatch(setLoadingStatus(true));
     dispatch(setOffersNearbyAction(data));
   }
 );
@@ -52,23 +69,19 @@ export const fetchCommentsAction = createAsyncThunk<void, number, {
 }>(
   'offers/fetchComments',
   async (id, { dispatch, extra: api }) => {
-    dispatch(setLoadingStatus(true));
     const { data } = await api.get<Reviews>(`${APIRoute.Reviews}/${id}`);
-    dispatch(setLoadingStatus(false));
     dispatch(setCommentsAction(data));
   },
 );
 
-export const fetchSendCommentAction = createAsyncThunk<void, {id: number; comment: string; rating: number}, {
+export const fetchSendCommentAction = createAsyncThunk<void, ReviewData, {
   dispatch: AppDispatch;
   state: State;
   extra: AxiosInstance;
 }>(
   'data/fetchComment',
   async ({id, comment, rating}, {dispatch, extra: api}) => {
-    dispatch(setLoadingStatus(true));
     await api.post(`${APIRoute.Reviews}/${id}`, {comment, rating});
-    dispatch(setLoadingStatus(false));
     dispatch(fetchCommentsAction(Number(id)));
   }
 );
@@ -96,9 +109,10 @@ export const loginAction = createAsyncThunk<void, AuthData, {
 }>(
   'user/login',
   async ({login: email, password}, {dispatch, extra: api}) => {
-    const {data: {token}} = await api.post<UserData>(APIRoute.Login, {email, password});
+    const {data: {token}, data} = await api.post<UserData>(APIRoute.Login, {email, password});
     saveToken(token);
     dispatch(requireAuthorization(AuthorizationStatus.Auth));
+    dispatch(getUserData(data));
     dispatch(redirectToRoute(AppRoute.Main));
   },
 );
